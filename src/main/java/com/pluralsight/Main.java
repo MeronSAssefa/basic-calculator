e7132158aca89bc798c60519ffda403f59accefc
MeronSAssefa <meronseife6@gmail.com>
2026-04-10 00:04:33 -0700

    second commit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
-public class Calculator {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double firstNumber = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        double secondNumber = scanner.nextInt();
+
+
+    }
 }
